57a13ef25ac88f921e5808d261bd9873da95033a
sanqbear <sanqbear@outlook.com>
2025-05-02 15:25:17 +0900

    feat: add module resolution for locales and refactor i18n imports

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         root: ['./src'],
         alias: {
           '@': './src',
+          '@locales': './src/i18n',
         },
       },
     ],
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import ko from './locales/ko.json';
-import en from './locales/en.json';
-import ja from './locales/ja.json';
-import zh from './locales/zh.json';
+import ko from '@locales/ko.json';
+import en from '@locales/en.json';
+import ja from '@locales/ja.json';
+import zh from '@locales/zh.json';
 
 i18n.use(initReactI18next).init({
   resources: {
