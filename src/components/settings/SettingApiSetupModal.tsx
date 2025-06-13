f8be1eb436a686843817657ff54edf7e0e50dbee
sanqbear <sanqbear@outlook.com>
2025-06-13 17:29:44 +0900

    feat(settings): API 주소 설정 모달 컴포넌트 추가 및 다국어 지원

---
+++ src/components/settings/SettingApiSetupModal.tsx
@@ -0,0 +1,111 @@
+import {darkTheme, lightTheme} from '@/types';
+import {useMemo} from 'react';
+import {
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import {TextInput} from 'react-native-gesture-handler';
+
+interface SettingApiSetupModalProps {
+  visible: boolean;
+  isDark: boolean;
+  value: string;
+  onSubmit: (url: string) => void;
+  onClose: () => void;
+}
+
+function SettingApiSetupModal({
+  visible,
+  isDark,
+  value,
+  onSubmit,
+  onClose,
+}: SettingApiSetupModalProps) {
+  const currentTheme = useMemo(() => {
+    return isDark ? darkTheme : lightTheme;
+  }, [isDark]);
+  const {t} = useTranslation();
+
+  return (
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={onClose}>
+      <Pressable
+        style={[styles.overlay, {backgroundColor: currentTheme.cardOverlay}]}
+        onPress={onClose}>
+        <View style={[styles.container, {backgroundColor: currentTheme.card}]}>
+          <Text
+            numberOfLines={1}
+            style={[styles.title, {color: currentTheme.text}]}>
+            {t('settings.setupApiUrl')}
+          </Text>
+          <TextInput
+            style={[
+              styles.input,
+              {
+                color: currentTheme.text,
+                borderColor: currentTheme.border,
+                backgroundColor: currentTheme.background,
+              },
+            ]}
+            value={value}
+            placeholder={t('settings.apiUrlPlaceholder')}
+            placeholderTextColor={currentTheme.textSecondary}
+          />
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor: currentTheme.primary}]}
+            onPress={() => onSubmit(value)}>
+            <Text
+              style={[styles.buttonText, {color: currentTheme.textPrimary}]}>
+              {t('settings.setupApiUrl')}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Pressable>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    width: '80%',
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    minWidth: 120,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
+
+export default SettingApiSetupModal;
